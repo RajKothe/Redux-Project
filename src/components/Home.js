f95d472672d95fe25ada89a7725386235a4f175b
RajKothe <rajkothe3300@gmail.com>
2024-01-10 12:43:35 +0530

    all file uploaded

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+// import iphone from './Iphone11.png';
+
+function Home(props) {
+        // const IphoneImage = "./Image/iphone11.png";
+    return (
+        <div>
+           
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                   <img src='../iphone11.png' /> 
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button 
+                    onClick={
+                        ()=>{props.addToCartHandler({pice:1000,name:'i phone 11'})}
+                        }>
+                        Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
